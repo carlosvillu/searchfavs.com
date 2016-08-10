c7344ab2214e39f98f6905936c197f4e4929c4b4
Gabriel Salas <ggsalas@gmail.com>
2016-08-10 19:06:09 -0300

    fix(style): style missed
    on jumbotron

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -7,13 +7,13 @@ const Signin = (_, {domain, factoryLogger}) => {
     domain.get('signin_with_twitter_user_use_case').execute()
   }
   return (
-    <div className="containerer">
-      <div className="jumbotron text-center">
+    <div className='containerer'>
+      <div className='jumbotron text-center'>
         <h1>searchfavs.com</h1>
         <p>Encontrá tus favoritos</p>
-        <button className='Signin btn btn-primary btn-lg' type="button" aria-label="Left Align">
-          <span  onClick={loginUser} >
-            <i className="fa fa-twitter" aria-hidden="true"></i>
+        <button className='Signin btn btn-primary btn-lg' type='button' aria-label='Left Align'>
+          <span onClick={loginUser} >
+            <i className='fa fa-twitter' aria-hidden='true'></i>
             &nbsp; Ingresar
           </span>
         </button>
