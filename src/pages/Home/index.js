d826d581c2f7ae046a60542651b9b95d2e45f896
Carlos Villuendas <carlosvillu@gmail.com>
2016-09-02 14:41:02 +0200

    refactor(Home): Create Home folder with the component and the sass

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Link from 'react-router/lib/Link'
 
-import ListTweets from '../components/ListTweets'
-import Search from '../components/Search'
+import ListTweets from '../../components/ListTweets'
+import Search from '../../components/Search'
 import './Home.scss'
 
 const Home = () => {
@@ -13,16 +13,16 @@ const Home = () => {
           <div className='container '>
             <div className='col-xs-12 col-md-8 col-md-offset-2 navbar-container'>
               <div className='navbar-logo hidden-xs hidden-sm'>
-                <a className="navbar-brand" href="#">searchfavs</a>
+                <a className='navbar-brand' href='#'>searchfavs</a>
               </div>
               <div className='navbar-search'>
                 <Search />
               </div>
               <div className='navbar-user'>
-               <button type='button' className='btn btn-default ' data-toggle='dropdown' data-target='#header-menu' aria-expanded='false'>
-                <span className='sr-only'>Toggle navigation</span>
-                <span className='glyphicon glyphicon-user'></span>{'\u00A0'}
-                </button>           
+                <button type='button' className='btn btn-default ' data-toggle='dropdown' data-target='#header-menu' aria-expanded='false'>
+                  <span className='sr-only'>Toggle navigation</span>
+                  <span className='glyphicon glyphicon-user' />{'\u00A0'}
+                </button>
                 <ul className='dropdown-menu' id='header-menu'>
                   <li role='presentation'><Link to='/logout'>Logout</Link></li>
                 </ul>
